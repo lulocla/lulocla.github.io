85b14cc6e1a3c206d56b4e56fadacf119e75c805
lulocla <166255445+lulocla@users.noreply.github.com>
2024-06-03 19:05:33 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -51,3 +51,23 @@ function setTheme(mode) {
 
     localStorage.setItem('theme', mode);
 }
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3700); // Change image every 3.7 seconds
+}
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
